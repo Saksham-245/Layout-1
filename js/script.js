4efb61642752fd6e3807cbc3e4c0c2311e1dc454
Saksham Mathur <Saksham-245@users.noreply.github.com>
2020-06-27 11:37:10 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function removeElement(){
+    var myObj = document.getElementById("div1");
+    myObj.remove();
+}
+
+function removeElement2(){
+    var myObj2 = document.getElementById("div2")
+    myObj2.remove();
+}
+function removeElement3(){
+    var myObj3 = document.getElementById("div3")
+    myObj3.remove();
+}
+function removeElement4(){
+    var myObj4 = document.getElementById("div4")
+    myObj4.remove();
+}
+function removeElement5(){
+    var myObj5 = document.getElementById("div5")
+    myObj5.remove();
+}
+
+function removeElement6(){
+    var myObj6 = document.getElementById("div6")
+    myObj6.remove();
+}
+function removeElement7(){
+    var myObj7 = document.getElementById("div7")
+    myObj7.remove();
+}
+
+function removeElement8(){
+    var myObj8 = document.getElementById("div8")
+    myObj8.remove();
+}
+
+function removeElement9(){
+    var myObj9= document.getElementById("div9")
+    myObj9.remove();
+}
